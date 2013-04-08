a3a236baa65552e6847140187d6eac82bbcb47b0
sara rubin <sararubin82@gmail.com>
2013-04-08 17:03:49 -0400

    nothing much, added the mapview, stupid simulator thinks i'm in sf

---
+++ FlagJack/FJMapViewController.h
@@ -0,0 +1,16 @@
+//
+//  FJMapViewController.h
+//  FlagJack
+//
+//  Created by srubin13 on 4/8/13.
+//  Copyright (c) 2013 Guerin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface FJMapViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
---
+++ FlagJack/FJMapViewController.m
@@ -0,0 +1,29 @@
+//
+//  FJMapViewController.m
+//  FlagJack
+//
+//  Created by srubin13 on 4/8/13.
+//  Copyright (c) 2013 Guerin. All rights reserved.
+//
+
+#import "FJMapViewController.h"
+
+@interface FJMapViewController ()
+
+@end
+
+@implementation FJMapViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
